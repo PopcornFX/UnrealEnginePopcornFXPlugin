1a98ab948822e04a8fde3d6a8deb0af34c15e28d
Hugo Nedelec <hnedelec@popcornfx.com>
2022-04-14 17:04:57 +0200

    PopcornFX Plugin v2.9.22

--- Download_SDK_Desktop.bat
+++ Download_SDK_Desktop.bat
@@ -6,7 +6,7 @@ setlocal
 
 bitsadmin /reset
 bitsadmin /create third_party_download_desktop
-bitsadmin /addfile third_party_download_desktop https://downloads.popcornfx.com/Plugins/UE4/UE4_PopcornFXPlugin_v2.9.21_Win64_Linux64_Mac64.7z "%~dp0\_PopcornFX_Runtime_SDK_Desktop.7z"
+bitsadmin /addfile third_party_download_desktop https://downloads.popcornfx.com/Plugins/UE4/UE4_PopcornFXPlugin_v2.9.22_Win64_Linux64_Mac64.7z "%~dp0\_PopcornFX_Runtime_SDK_Desktop.7z"
 bitsadmin /setpriority third_party_download_desktop "FOREGROUND"
 bitsadmin /resume third_party_download_desktop
 
@@ -14,6 +14,8 @@ bitsadmin /resume third_party_download_desktop
     call bitsadmin /info third_party_download_desktop /verbose | find "STATE: TRANSFERRED"
     if %ERRORLEVEL% equ 0 goto WAIT_FOR_DOWNLOAD_LOOP_END
     call bitsadmin /RawReturn /GetBytesTransferred third_party_download_desktop
+    printf ' / '
+    call bitsadmin /RawReturn /GetBytesTotal third_party_download_desktop
     echo Bytes transferred
     timeout 2 > nul
     goto WAIT_FOR_DOWNLOAD_LOOP_START
@@ -24,8 +26,8 @@ bitsadmin /complete third_party_download_desktop
 echo "---------- Download complete ----------"
 echo "---------- Preparing PopcornFX_Runtime_SDK/ ----------"
 
-rem rmdir /s /q %~dp0\PopcornFX_Runtime_SDK
-mkdir %~dp0\PopcornFX_Runtime_SDK
+rem rmdir /s /q "%~dp0\PopcornFX_Runtime_SDK"
+mkdir "%~dp0\PopcornFX_Runtime_SDK"
 
 echo "---------- Unzipping _PopcornFX_Runtime_SDK.7z ----------"
 
@@ -33,11 +35,11 @@ echo "---------- Unzipping _PopcornFX_Runtime_SDK.7z ----------"
 
 echo "---------- Copying PopcornFX Runtime SDK ----------"
 
-robocopy _PopcornFX_Runtime_SDK_Desktop\UE4PopcornFXPlugin\PopcornFX\PopcornFX_Runtime_SDK %~dp0\PopcornFX_Runtime_SDK /s
+robocopy _PopcornFX_Runtime_SDK_Desktop\PopcornFX\PopcornFX_Runtime_SDK "%~dp0\PopcornFX_Runtime_SDK" /s
 
 echo "---------- Removing temp files ----------"
 
-rmdir /s /q %~dp0\_PopcornFX_Runtime_SDK_Desktop
+rmdir /s /q "%~dp0\_PopcornFX_Runtime_SDK_Desktop"
 del /f _PopcornFX_Runtime_SDK_Desktop.7z
 
 echo "---------- Finished ----------"
